e625d6e2ccafa2114c2a779c7600300351c53cb3
Sam <63341022+whereissam@users.noreply.github.com>
2021-01-23 15:20:07 +0800

    Custom event
    Custom class In style.css add class:{ @apply xxxxx }

---
+++ Tailwind foodmune/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: { //add to 
+      colors:{
+        primary: '#aabbcc',
+        secondary:{
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
